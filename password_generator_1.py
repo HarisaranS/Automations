7738a71b31e5865a5469fc19ef118df8ab415f5b
Harisaran <harisaran777s@gmail.com>
2025-10-21 12:06:14 +0530

    updated Password generator

---
+++ password_generator_1.py
@@ -0,0 +1,20 @@
+import string 
+import random 
+
+def passgen():
+
+    s1 = string.ascii_lowercase
+    s2 = string.ascii_uppercase
+    s3 = string.digits
+    s4 = string.punctuation
+    passlen = int(input("Enter the length of the password:"))
+    s = []
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+    random.shuffle(s)
+    password = ("".join(s[0:passlen]))
+    print(password)
+
+passgen()

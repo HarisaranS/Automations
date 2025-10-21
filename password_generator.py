24ceb55ed72d5cf70906b07877a1fb110624beb7
Harisaran <harisaran777s@gmail.com>
2025-10-21 12:18:10 +0530

    Updated password generator

--- password_generator.py
+++ password_generator.py
@@ -1,5 +1,6 @@
 import string 
 import random 
+from csv  import writer 
 
 def passgen():
 
@@ -7,6 +8,7 @@ def passgen():
     s2 = string.ascii_uppercase
     s3 = string.digits
     s4 = string.punctuation
+    platform = input("Enter the platform for which you want to generate the password:") 
     passlen = int(input("Enter the length of the password:"))
     s = []
     s.extend(list(s1))
@@ -16,5 +18,9 @@ def passgen():
     random.shuffle(s)
     password = ("".join(s[0:passlen]))
     print(password)
+    pass_data = [platform, password]
+    with open("path/to/csv_file",'a') as f:
+        write_data = writer(f)
+        write_data.writerow(pass_data)
 
 passgen()

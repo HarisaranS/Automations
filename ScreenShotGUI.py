fb7f2c5fede99876436154b975af0f8a549d88d4
Harisaran <harisaran777s@gmail.com>
2025-10-21 11:05:47 +0530

    updated ScreenShotGUI Application using tkinter

--- ScreenShotGUI.py
+++ ScreenShotGUI.py
@@ -1,10 +1,22 @@
 import pyautogui
 import time
+import tkinter as tk
 
 def screenshot():
-    time.sleep(3)
+    time.sleep(0)
+    name = f'/home/saran/Pictures/Screenshots/{time.time()}.png'
     img = pyautogui.screenshot()
-    img.save("screenshot.png")
+    img.save(name)
     img.show()
 
-screenshot()
+root =tk.Tk()
+root.title("Screenshot GUI")
+
+
+btn = tk.Button(root,text="Take Screenshot",command=screenshot) 
+btn.pack(side=tk.LEFT,pady=10,padx=5)
+
+close = tk.Button(root,text="Quit",command=quit)
+close.pack(side=tk.LEFT,pady=10,padx=5)
+
+root.mainloop()
